914759f677231c6c02fd84faf8e2cb80dcd1852c
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-07-12 00:26:33 +0530

    Added the quiz

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ from fastapi import FastAPI,File,UploadFile,Form
 from pydantic import BaseModel
 from typing_extensions import Annotated
 
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
+from youtube_transcript_api.formatters import TextFormatter
+
+
 import os
 from groq import Groq
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import StorageContext, load_index_from_storage
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+import random
+import re
+
+from sklearn.cluster import KMeans
+import numpy as np
+
+
+
 
 from docx import Document
 from PyPDF2 import PdfReader
@@ -15,6 +41,8 @@ UPLOAD_DIR = Path() / "upload"
 client = Groq(
     api_key="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",
 )
+
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 
 app = FastAPI()
@@ -32,25 +60,6 @@ def read_root():
     print("Health Check")
     return {"status": "ok"}
 
-    if docs.data == "":
-        return {"error": "File is required"}
-    else: 
-
-        content = "Role: You are the Q&A solver. Here is your information: Data: " + docs.data + "Using this information, answer the following question: Question:"+ docs.userPrompt + "Instruction: Answer the question using the information provided in the data."
-        try : 
-            chat_completion = client.chat.completions.create(
-                messages=[
-                    {
-                        "role": "user",
-                        "content": content,
-                    }
-                ],
-                model="llama3-8b-8192",
-            )
-            return chat_completion.choices[0].message.content
-        except :
-            return {"error": str(e)}
-
 
 def extract_text_from_pdf(file_path):
     reader = PdfReader(file_path)
@@ -58,18 +67,15 @@ def extract_text_from_pdf(file_path):
     text = ""
     for page in reader.pages:
         text += page.extract_text()
-    print(text)
     os.remove(file_path)
     return text
 
 def extract_text_from_docx(file_path):
     doc = Document(file_path)
-    print("Docs File Read")
     para = doc.paragraphs
     text = ""
     for p in para:
         text += p.text
-    print(text)
     os.remove(file_path)
     return text
 
@@ -78,6 +84,28 @@ def extract_text_from_txt(file_path):
         text = f.read()
     os.remove(file_path)
     return text
+
+class DocumentStore:
+    def __init__(self):
+        self.documents = []
+        self.vectorizer = TfidfVectorizer()
+        self.document_matrix = None
+
+    def add_document(self, text):
+        new_chunks = chunk_text(text)
+        self.documents.extend(new_chunks)
+        self.document_matrix = self.vectorizer.fit_transform(self.documents)
+
+    def query(self, userPrompt, top_k=5):
+        if not self.documents:
+            return []
+
+        query_vector = self.vectorizer.transform([userPrompt])
+        similarities = cosine_similarity(query_vector, self.document_matrix).flatten()
+        indices = similarities.argsort()[-top_k:][::-1]
+        return [self.documents[i] for i in indices]
+
+store = DocumentStore()
 
 @app.post("/summarize")
 async def file_summary(
@@ -99,22 +127,34 @@ async def file_summary(
         else:
             return {"error": "Unsupported file type"}
         
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000,
+            length_function=len,
+            is_separator_regex=False,
+        )
+
+        chunk = text_splitter.create_documents([text])
+
+        combined_text = ""
+        for i in range(4):
+            combined_text += str(random.choice(chunk))
+
+        
         content = (
             f"Instruction: You are a summary generator, your job is to generate a summary of the given data. "
             f"You have to follow the instructions given on how to generate the summary. If no instruction is given, "
-            f"then just generate the summary. Data: {text} Instruction: {userPrompt} "
+            f"then just generate the summary. Data: {combined_text} Instruction: {userPrompt} "
             f"Instruction: The summary should be at least 200 words long."
         )
 
         chat_completion = client.chat.completions.create(
             messages=[{"role": "user", "content": content}],
-            model="llama3-8b-8192",
+            model="llama3-70b-8192",
         )
 
         return chat_completion.choices[0].message.content
     except Exception as e:
         return {"error": str(e)}
-
 
 
 @app.post("/chat")
@@ -137,15 +177,116 @@ async def file_chat(
         else:
             return {"error": "Unsupported file type"}
 
+            # load_index_from_storage() missing 1 required positional argument: 'storage_context'
+
+        index = VectorStoreIndex.from_vector_store(vector_store)
+        query_engine = index.as_query_engine()
+
+        response = query_engine.query(userPrompt)
+
         content = (
-            f"Instruction: You are a Q&A solver. Here is your information: Data: {text} Using this information, answer the following question: Question: {userPrompt} Instruction: Answer the question using the information provided in the data."
+            f"Instruction: You are a Q&A solver. Here is your information: Data: {response} Using this information, answer the following question: Question: {userPrompt} Instruction: Answer the question using the information provided in the data."
         ) 
 
         chat_completion = client.chat.completions.create(
             messages=[{"role": "user", "content": content}],
-            model="llama3-8b-8192",
+            model="llama3-70b-8192",
         )
 
         return chat_completion.choices[0].message.content
     except Exception as e:
         return {"error": str(e)}
+
+
+
+@app.post("/quiz")
+async def quiz(
+    upload_file: Annotated[UploadFile, File()]):
+    try:
+        file_read = await upload_file.read()
+        file_path = os.path.join(UPLOAD_DIR, upload_file.filename)
+        os.makedirs(UPLOAD_DIR, exist_ok=True)
+        with open(file_path, "wb") as f:
+            f.write(file_read)
+
+        if upload_file.filename.endswith(".pdf"):
+            text = extract_text_from_pdf(file_path)
+        elif upload_file.filename.endswith(".docx"):
+            text = extract_text_from_docx(file_path)
+        elif upload_file.filename.endswith(".txt"):
+            text = extract_text_from_txt(file_path)
+        else:
+            return {"error": "Unsupported file type"}
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000,
+            length_function=len,
+            is_separator_regex=False,
+        )
+
+        chunk = text_splitter.create_documents([text])
+
+        combined_text = ""
+        for i in range(4):
+            combined_text += str(random.choice(chunk))
+
+
+        content = (
+            f"Generate a quiz based on the following information: Data: {combined_text} "
+            f"Instructions :"
+            f"1. Generate a quiz based on the given information."
+            f"2. The quiz should be at least 10 questions long."
+            f"3. The quiz should be in the form of a list of questions and options."
+            f"Format of the quiz:"
+            f"Each question should be start with **Question :***"
+            f"Option should be start with **Option :***"
+            f"Each answer should be like **Answer :*** and only give the option number for the answer"
+            f"Options: 1, 2, 3, 4"
+            f"Answer: Answer"
+        ) 
+
+        chat_completion = client.chat.completions.create(
+            messages=[{"role": "user", "content": content}],
+            model="llama3-70b-8192",
+        )
+
+        
+        def parse_quiz(response):
+            question_blocks = re.split(r"\*\*Question \d+:\*\*", response)[1:]
+            parsed_quiz = []
+
+            for block in question_blocks:
+                question_match = re.search(r"^(.*?)\n", block)
+                question_text = question_match.group(1).strip() if question_match else block.strip()
+                
+                options = re.findall(r"\*\*Option (\d+):\*\* (.*?)\n", block)
+                formatted_options = [f"{opt[1]}" for opt in options]
+
+                
+                answer_match = re.search(r"\*\*Answer:\*\* (\d+)", block)
+                answer_option = answer_match.group(1) if answer_match else ""
+
+                parsed_quiz.append({
+                    "question": question_text,
+                    "options": formatted_options,
+                    "answer": answer_option
+                })
+
+            return parsed_quiz
+
+        
+        print(chat_completion.choices[0].message.content)
+        # print the parsed quiz its array of questions and options
+        json_format =  parse_quiz(chat_completion.choices[0].message.content) 
+
+
+        json_compatible_item_data = jsonable_encoder(json_format)
+        print(json_compatible_item_data)
+        return JSONResponse(content=json_compatible_item_data)
+
+    except Exception as e:
+        print(e)
+        return {"error": str(e)}
+
+        
+        
