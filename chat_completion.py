bef32973410c9622f1564d4fe04d67c29bc18be8
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-08-06 22:21:45 +0530

    fix

--- chat_completion.py
+++ chat_completion.py
@@ -14,7 +14,7 @@ payload = {
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
-    "authorization": "Bearer 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"
+    "authorization": "Bearer sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 response = requests.post(url, json=payload, headers=headers)
